82106c8202199249b8ef38b6676bacfcb66b4fe6
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-22 16:25:33 +0500

    topic => numbers

--- js/basics/script.js
+++ js/basics/script.js
@@ -682,5 +682,46 @@ let str = "Привет";
 str.test = 5; // примитивы => не являются объектами
 
 alert(str.test);
+
+
+let a = +prompt("Введите первое число", 0);
+let b = +prompt("Введите второе число", 0);
+
+alert(a+b);
+
+alert( Math.round(6.35 * 10) / 10 ); // 6.35 -> 63.5 -> 64(rounded) -> 6.4
+
+function readNumber() {
+    let num;
+    do{
+        num = prompt("Ваше число", 0);
+
+    } while(!isFinite(num));
+    if (num === null || num === '') return null;
+    return +num;
+}
+alert(`Число : ${readNumber()}`);
+
+↓ Цикл ломается i на последней итерации равно 9,999999999999999999982 но не 10
+let i = 0;
+while (i != 10) {
+  i += 0.2;
+}
+alert(i);
+
+function random(min, max) {
+    return min + Math.random() * (max - min);
+  }
+  
+  alert( random(1, 5) );
+  alert( random(1, 5) );
+  alert( random(1, 5) );
+
+  function randomInteger(min, max){
+     let rand = min + Math.random() * (max + 1 - min);
+     return Math.floor(rand);
+  }
+  alert(randomInteger(1,3));
+
  */
 
