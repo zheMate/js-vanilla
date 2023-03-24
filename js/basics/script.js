7b23125a5a4f84c7c76bf5e2ef5d720011e7c5c4
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-24 12:16:48 +0500

    destructing-assignment

--- js/basics/script.js
+++ js/basics/script.js
@@ -1084,4 +1084,35 @@ function count(obj) {
 }
 
 alert(count(user));
+
+let user = {
+    name: "John",
+    years: 30,
+};
+
+let{name, years: age, isAdmin = false} = user;
+
+alert(name);
+alert(age);
+alert(isAdmin);
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250,
+};
+
+function topSalary(salaries) {
+    let max = 0;
+    let maxName = null;
+    for (const [name, salary] of Object.entries(salaries)) {
+        if (max < salary) {
+            max = salary;
+            maxName = name;
+        }
+    }
+    return alert(`Человек с самой высокой зарплатой => ${maxName}`);
+}
+
+topSalary(salaries);
  */
