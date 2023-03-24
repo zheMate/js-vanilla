12d95a3ad053f4306e6d593e7bd27458027b4738
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-24 11:31:40 +0500

    Object.keys, values, entries

--- js/basics/script.js
+++ js/basics/script.js
@@ -1059,4 +1059,29 @@ let readMap = new WeakMap();
 
 readMap.set(messages[0], new Date(2017, 1, 1));
 
+function sumSalaries(salaries) {
+    let sum = 0; 
+    for(let salary of Object.values(salaries)) {
+        sum += salary;
+    }
+    return sum;
+}
+let salaries =  {
+    "John": 100, 
+    "Pete": 300,
+    "Mary": 250,
+};
+
+alert(sumSalaries(salaries));
+
+let user = {
+    name: 'John',
+    age: 30,
+};
+
+function count(obj) {
+    return Object.keys(obj).length;
+}
+
+alert(count(user));
  */
