11183f01331d42e2aab3918764a334fc478a7493
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-24 10:34:14 +0500

    map and set

--- js/basics/script.js
+++ js/basics/script.js
@@ -1001,4 +1001,34 @@ let users = [
 let userById = groupById(users);
 console.log(userById);
 
+function unique(arr){
+    return Array.from(new Set(arr));
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert(unique(values));
+
+function aclean(arr) {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        let sorted = arr[i]
+            .toLowerCase()
+            .split("")
+            .sort()
+            .join("");
+        obj[sorted] = arr[i];
+    }
+    return Object.values(obj);
+}
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+alert(aclean(arr));
+
+let map = new Map();
+map.set("name", "John");
+let keys = Array.from(map.keys());
+keys.push("more");
+alert(keys);
  */
