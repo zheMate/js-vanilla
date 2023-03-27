eeddeb88ebe53e9738f9d23d2a3c8dbc1ecd4696
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-27 12:25:16 +0500

    function-object

--- js/basics/script.js
+++ js/basics/script.js
@@ -745,5 +745,32 @@ function makeArmy() {
   
   army[0](); // 0
   army[5](); // 5
-
+  
+  function makeCounter() {
+      let count = 0;
+      function counter() {
+          return count++;
+      }
+      counter.set = value => count = value;
+      counter.decrease = () => count--;
+      return counter;
+  }
+  
+  function sum(a) {
+      let currentSum = a;
+      function f(b) {
+          currentSum += b;
+          return f;
+      }
+      f.toString = function () {
+          return currentSum;
+      };
+      return f;
+  }
+  
+  alert(sum(1)(2));
+  alert(sum(5)(-1)(2));
+  alert(sum(6)(-1)(-2)(-3));
+  alert(sum(0)(1)(2)(3)(4)(5)); 
+  
  */
