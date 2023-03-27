a04388056fbbf3ee0e792f27f44123d09fb8c533
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-27 13:45:07 +0500

    bind

--- js/basics/script.js
+++ js/basics/script.js
@@ -876,5 +876,70 @@ function spy(func) {
   
     return wrapper;
   }
+
+  function f() {
+    alert( this ); // null
+  }
+  
+  let user = {
+    g: f.bind(null)
+  };
+  
+  user.g();
+  function f() {
+    alert(this.name);
+  }
+  
+  f = f.bind( {name: "Вася"} ).bind( {name: "Петя"} );
+  
+  f(); // Вася
+
+  function sayHi() {
+    alert( this.name );
+  }
+  sayHi.test = 5;
+  
+  let bound = sayHi.bind({
+    name: "Вася"
+  });
+  
+  alert( bound.test ); // что выведет? почему?
+  //вывод => undefined, тк у bound нет свойства test
+  function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user = {
+    name: 'Вася',
+  
+    loginOk() {
+      alert(`${this.name} logged in`);
+    },
+  
+    loginFail() {
+      alert(`${this.name} failed to log in`);
+    },
+  
+  };
+  
+  askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+  function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user = {
+    name: 'John',
+  
+    login(result) {
+      alert( this.name + (result ? ' logged in' : ' failed to log in') );
+    }
+  };
+  
+  askPassword(() => user.login(true), () => user.login(false));
  */
 
