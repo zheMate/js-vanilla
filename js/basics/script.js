f0423e16b873241c27790d81445cbc35edb03d4e
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-28 11:34:43 +0500

    promise-basics

--- js/basics/script.js
+++ js/basics/script.js
@@ -1023,5 +1023,52 @@ alert(err.message);
 alert(err.name);
 alert(err.stack);
 alert(err instanceof SyntaxError);
+
+let promise = new Promise(function(resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(alert); //Вывод 1 второй resolve будет проигнорирован
+ поскольку учитывается только первый вызов reject/resolve, 
+ все последущие вызовы игнорируются 
+ 
+ function delay(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+ }
+ 
+ delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+
+   function go() {
+    showCircle(150, 150, 100).then(div => {
+      div.classList.add('message-ball');
+      div.append("Hello, world!");
+    });
+  }
+
+  function showCircle(cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+
+        div.addEventListener('transitionend', function handler() {
+          div.removeEventListener('transitionend', handler);
+          resolve(div);
+        });
+      }, 0);
+    })
+  }
+  
  */
 
