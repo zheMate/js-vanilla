5c4a6a168f47ea919bb148057125a701d4766ed7
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-28 13:20:54 +0500

    async-await

--- js/basics/script.js
+++ js/basics/script.js
@@ -1070,5 +1070,59 @@ promise.then(alert); //Вывод 1 второй resolve будет проигн
     })
   }
   
+  async function loadJson(url) {
+    let response = await fetch(url);
+    if(response.status == 200) {
+      let json = await response.json();
+      return json;
+    }
+    throw new Error(response.status);
+  }
+  loadJson('no-such-user.json').catch(alert);
+  
+  class HttpError extends Error {
+    constructor(response) {
+      super(`${response.status} for ${response.url}`);
+      this.name = 'HttpError';
+      this.response = response;
+    }
+  }
+  async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+      return response.json();
+    }
+    else {
+      throw new HttpError(response);
+    }
+  }
+  async function demoGitHubUser() {
+   let user;
+   while(true) {
+    let name = prompt("Введите логин ?", "iliakan");
+    try{
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break;
+    }catch(err){
+      if(err instanceof HttpError && err.response.status == 404) {
+        alert("Такого пользователя не сущетсвует, пожалуйста, повторите ввод.");
+      }
+      else{ 
+        throw err;
+      }
+    }
+   }
+   alert(`Полное имя : ${user.name}`);
+   return user;
+  }
+  
+  demoGitHubUser();
+  
+  async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return 10;
+  }
+  function f(){
+    wait().then(result => alert(result));
+  }
  */
-
