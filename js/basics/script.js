788e0f6d17ae3b6e44b00a8437e183ba6adde8e3
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-29 10:27:21 +0500

    styles-and-classses

--- js/basics/script.js
+++ js/basics/script.js
@@ -1351,4 +1351,33 @@ let sortedRows = Array.from(table.rows)
   .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
 
 table.tBodies[0].append(...sortedRows);
+
+ function showNotification({top = 0, right = 0, className, html}) {
+
+      let notification = document.createElement('div');
+      notification.className = "notification";
+      if (className) {
+        notification.classList.add(className);
+      }
+
+      notification.style.top = top + 'px';
+      notification.style.right = right + 'px';
+
+      notification.innerHTML = html;
+      document.body.append(notification);
+
+      setTimeout(() => notification.remove(), 1500);
+    }
+
+    // test it
+    let i = 1;
+    setInterval(() => {
+      showNotification({
+        top: 10,
+        right: 10,
+        html: 'Hello ' + i++,
+        className: "welcome"
+      });
+    }, 2000);
+    
  */
