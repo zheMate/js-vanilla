66aa4ee5dbd9c057cbfc31523275957d115dc413
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-29 14:05:16 +0500

    form-elements

--- js/basics/script.js
+++ js/basics/script.js
@@ -1778,4 +1778,14 @@ function showVisible() {
 
 showVisible();
 window.onscroll = showVisible;
+
+  // 1)
+let selectedOption = genres.options[genres.selectedIndex];
+  alert( selectedOption.value );
+  alert( selectedOption.text );
+  // 2)
+  let newOption = new Option("Классика", "classic");
+  genres.append(newOption);
+  // 3)
+  newOption.selected = true;
  */
