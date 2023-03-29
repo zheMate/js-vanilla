105a3b92cf449560ad56ecd75919d82511edff52
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-29 09:03:37 +0500

    searching-elements-dom

--- js/basics/script.js
+++ js/basics/script.js
@@ -1145,4 +1145,34 @@ let table = document.body.firstElementChild;
       let row = table.rows[i];
       row.cells[i].style.backgroundColor = 'red';
     }
+
+    // 1. Таблица с `id="age-table"`.
+let table = document.getElementById('age-table')
+
+// 2. Все label в этой таблице
+table.getElementsByTagName('label')
+// или
+document.querySelectorAll('#age-table label')
+
+// 3. Первый td в этой таблице
+table.rows[0].cells[0]
+// или
+table.getElementsByTagName('td')[0]
+// или
+table.querySelector('td')
+
+// 4. Форма с name="search"
+// предполагаем, что есть только один элемент с таким name в документе
+let form = document.getElementsByName('search')[0]
+// или, именно форма:
+document.querySelector('form[name="search"]')
+
+// 5. Первый input в этой форме
+form.getElementsByTagName('input')[0]
+// или
+form.querySelector('input')
+
+// 6. Последний input в этой форме
+let inputs = form.querySelectorAll('input') // найти все input
+inputs[inputs.length-1] // взять последний
  */
