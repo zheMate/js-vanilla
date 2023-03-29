ac5db2155a00cd92fd0c96527b4804ee6182cf26
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-29 13:22:36 +0500

    default-browser-action

--- js/basics/script.js
+++ js/basics/script.js
@@ -1675,4 +1675,14 @@ table.tBodies[0].append(...sortedRows);
       }
 
     };
+
+     thumbs.onclick = function(event) {
+      let thumbnail = event.target.closest('a');
+      if (!thumbnail) return;
+      showThumbnail(thumbnail.href, thumbnail.title);
+      event.preventDefault();
+    }
+    function showThumbnail(href, title) {
+      largeImg.src = href;
+      largeImg.alt = title;
  */
