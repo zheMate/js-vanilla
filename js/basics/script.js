57c44607354f64f6aa017fac020a4f65c1d9a5ed
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-29 11:56:25 +0500

    introduction-browser-events

--- js/basics/script.js
+++ js/basics/script.js
@@ -1488,4 +1488,87 @@ table.tBodies[0].append(...sortedRows);
     showNote(blockquote, "top-out", "note top-out");
     showNote(blockquote, "right-out", "note right-out");
     showNote(blockquote, "bottom-in", "note bottom-in");
+
+    document.getElementById('hider').onclick = function() {
+      document.getElementById('text').hidden = true;
+    }
+
+    <input type="button" onclick="this.hidden=true" value="Нажми, чтобы спрятать">
+
+    field.onclick = function(event) {
+
+      // координаты поля относительно окна браузера
+      let fieldCoords = this.getBoundingClientRect();
+
+      // мяч имеет абсолютное позиционирование (position:absolute), поле - относительное (position:relative)
+      // таким образом, координаты мяча рассчитываются относительно внутреннего, верхнего левого угла поля
+      let ballCoords = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+      };
+
+      // запрещаем пересекать верхнюю границу поля
+      if (ballCoords.top < 0) ballCoords.top = 0;
+
+      // запрещаем пересекать левую границу поля
+      if (ballCoords.left < 0) ballCoords.left = 0;
+
+
+      // // запрещаем пересекать правую границу поля
+      if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+        ballCoords.left = field.clientWidth - ball.clientWidth;
+      }
+
+      // запрещаем пересекать нижнюю границу поля
+      if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+        ballCoords.top = field.clientHeight - ball.clientHeight;
+      }
+
+      ball.style.left = ballCoords.left + 'px';
+      ball.style.top = ballCoords.top + 'px';
+    }
+
+     let menuElem = document.getElementById('sweeties');
+     let titleElem = menuElem.querySelector('.title');
+      titleElem.onclick = function() {
+      menuElem.classList.toggle('open');
+    };
+
+    let panes = document.querySelectorAll('.pane');
+    for(let pane of panes) {
+      pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+      // кнопка становится первым потомком плитки (pane)
+      pane.firstChild.onclick = () => pane.remove();
+    }
+    let i = 1;
+    for(let li of carousel.querySelectorAll('li')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
+
+    let width = 130; // ширина картинки
+    let count = 3; // видимое количество изображений
+
+    let list = carousel.querySelector('ul');
+    let listElems = carousel.querySelectorAll('li');
+
+    let position = 0; // положение ленты прокрутки
+
+    carousel.querySelector('.prev').onclick = function() {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+    
  */
