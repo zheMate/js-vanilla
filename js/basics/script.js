d08f8a65bdaee2cab613438921bbf5741b72e9c2
Вуколов Данил Александрович <vukolov.da@pit.local>
2023-03-29 13:29:59 +0500

    mouse-events-basics

--- js/basics/script.js
+++ js/basics/script.js
@@ -1685,4 +1685,30 @@ table.tBodies[0].append(...sortedRows);
     function showThumbnail(href, title) {
       largeImg.src = href;
       largeImg.alt = title;
+
+       ul.onclick = function(event) {
+      if (event.target.tagName != "LI") return;
+      if (event.ctrlKey || event.metaKey) {
+        toggleSelect(event.target);
+      } else {
+        singleSelect(event.target);
+      }
+    }
+    // предотвращает ненужное выделение элементов списка при клике
+    ul.onmousedown = function() {
+      return false;
+    };
+
+    function toggleSelect(li) {
+      li.classList.toggle('selected');
+    }
+
+    function singleSelect(li) {
+      let selected = ul.querySelectorAll('.selected');
+      for(let elem of selected) {
+        elem.classList.remove('selected');
+      }
+      li.classList.add('selected');
+    }
+    
  */
